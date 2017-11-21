6faecbdb48dd0177f2bee444b878f8c91691c6cf
hygor <hygor_christian@hotmail.com>
2017-11-21 15:58:26 -0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TituloComponent } from './titulo/titulo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TituloComponent
   ],
   imports: [
     BrowserModule
